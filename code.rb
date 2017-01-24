4ce9bef10c1ccfaa76ba99dd3648a2b48ae459e2
dhill92 <dhill92@gmail.com>
2017-01-24 17:37:21 -0500

    first commit

---
+++ code.rb
@@ -0,0 +1,10 @@
+#Your code goes here
+
+size = "extra large"
+name = "jack"
+color = "blue"
+temp = "chilly"
+rides = "exhilirating"
+
+puts "My friends and I had a great day at the park. I got a #{size} sundae, and it was pretty good. It was #{name} screamed the loudest
+on the tilt-a-whirl. For a snack we got #{color} cotton candy. The wave pool was #{temp}. The rides were all #{rides}."
